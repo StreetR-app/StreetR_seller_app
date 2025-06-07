6dfd4f7a293e19e32f95e33c195d38b00948b75f
StreetR-app <StreetR143@gmail.com>
2025-06-07 08:29:15 +0530

    Update js_supabaseClient.js
    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkbWxiZWRhY3ltb3djZ3R5a3ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNjMwOTUsImV4cCI6MjA2NDgzOTA5NX0.oQ98WG4hXtlwTUaj5Aj9R6UVHPZvgR0iQnhxuVtVfRc
    
    
    https://qdmlbedacymowcgtykvm.supabase.co

--- js_supabaseClient.js
+++ js_supabaseClient.js
@@ -1,6 +1,6 @@
 // js/supabaseClient.js
-const SUPABASE_URL = 'https://rnjvqxdrvplgilqzwnpl.supabase.co'; // Replace with your Supabase URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuanZxeGRydnBsZ2lscXp3bnBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NDU4NjYsImV4cCI6MjA2NDUyMTg2Nn0.IOAxp8ULZgccX8hKtlDQzwdrW7xp1CcXVSdJ59UEruA'; // Replace with your Supabase Anon Key
+const SUPABASE_URL = 'https://qdmlbedacymowcgtykvm.supabase.co'; // Replace with your Supabase URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkbWxiZWRhY3ltb3djZ3R5a3ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNjMwOTUsImV4cCI6MjA2NDgzOTA5NX0.oQ98WG4hXtlwTUaj5Aj9R6UVHPZvgR0iQnhxuVtVfRc'; // Replace with your Supabase Anon Key
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     console.error("Supabase URL or Anon Key is missing. Please check your supabaseClient.js file.");
